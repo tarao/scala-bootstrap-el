fb5a447e0815c539f998889e29c4e4a3fba37fbc
tarao <tarao.gnn@gmail.com>
2019-06-23 22:27:08 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,92 @@
+scala-bootstrap.el
+==================
+
+A bootstrapping installer for Scala tools.
+
+Synopsis
+--------
+
+This package provides an installer of Scala tools, which is convenient
+if you are using [lsp-mode][].
+
+```elisp
+(require 'scala-bootstrap)
+(require 'lsp)
+
+(add-hook 'scala-mode-hook
+          '(lambda ()
+             (scala-bootstrap:with-metals-installed
+              (scala-bootstrap:with-bloop-server-started
+               (lsp)))))
+```
+
+Macros
+------
+
+### `scala-bootstrap:with-metals-installed` (`&rest body`)
+
+Ensures that [Metals][] is installed.  It is installed to a directory
+specified by `scala-bootstrap:bin-directory`, which defaults to
+`~/.emacs.d/bin`.
+
+If `scala-bootstrap:metals-version` is non-nill, that version of
+[Metals][] will be installed.  Otherwise, the latest version will be
+installed.
+
+It also installs [Coursier][] binary in background, which is needed to
+install [Metals][].
+
+### `scala-bootstrap:with-bloop-installed` (`&rest body`)
+
+Ensures that [Bloop][] is installed.  It is installed to a directory
+specified by `scala-bootstrap:bin-directory`, which defaults to
+`~/.emacs.d/bin`.
+
+If `scala-bootstrap:bloop-version` is non-nill, that version of
+[Bloop][] will be installed.  Otherwise, the latest version will be
+installed.
+
+### `scala-bootstrap:with-bloop-server-started` (`&rest body`)
+
+Ensures that [Bloop][] server is started.  If there is one somewhere
+on your machine, it does nothing.  Otherwise, a server will be started
+as a process which appears in `M-x list-processes` and whose logs
+appear in `*bloop-server*` buffer.
+
+This macro also ensures that [Bloop][] is installed before starting a
+server.
+
+Funcitions
+----------
+
+### `scala-bootstrap:reinstall-metals` ()
+
+Reinstalls [Metals][].
+
+### `scala-bootstrap:reinstall-bloop` ()
+
+Reinstalls [Bloop][].
+
+Variables
+---------
+
+### `scala-bootstrap:bin-directory`
+
+Directory to place installed tools.
+
+### `scala-bootstrap:metals-scala-version`
+
+Scala version of [Metals][].
+
+### `scala-bootstrap:metals-version`
+
+[Metals][] version to install.
+
+### `scala-bootstrap:bloop-version`
+
+[Bloop][] version to install.
+
+[Metals]: https://scalameta.org/metals/
+[Bloop]: https://scalacenter.github.io/bloop/
+[Coursier]: https://get-coursier.io/
+[lsp-mode]: https://github.com/emacs-lsp/lsp-mode
---
+++ scala-bootstrap.el
@@ -0,0 +1,304 @@
+;;; scala-bootstrap.el --- Installer of Scala tools
+
+;; Author: INA Lintaro <tarao.gnn at gmail.com>
+;; URL: https://github.com/tarao/scala-bootstrap-el
+;; Version: 0.1
+;; Keywords: scala tool
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'json)
+
+(defgroup scala-bootstrap nil
+  "Installer of Scala tools"
+  :prefix "scala-bootstrap:"
+  :group 'convenience)
+
+(defcustom scala-bootstrap:bin-directory
+  (expand-file-name "bin" user-emacs-directory)
+  "Directory to place installed tools."
+  :type 'directory
+  :group 'scala-bootstrap)
+
+(defcustom scala-bootstrap:metals-scala-version "2.12"
+  "Scala version of Metals."
+  :type 'string
+  :group 'scala-bootstrap)
+
+(defcustom scala-bootstrap:metals-version nil
+  "Metals version to install."
+  :type '(choice (const :tag "latest" nil) string)
+  :group 'scala-bootstrap)
+
+(defcustom scala-bootstrap:bloop-version nil
+  "Bloop version to install."
+  :type '(choice (const :tag "latest" nil) string)
+  :group 'scala-bootstrap)
+
+(defconst scala-bootstrap-metals-artifact-template
+  "org.scalameta:metals_%s:%s")
+(defconst scala-bootstrap-metals-releases-api
+  "https://api.github.com/repos/scalameta/metals/releases/latest")
+(defconst scala-bootstrap-bloop-installer-url-template
+  "https://github.com/scalacenter/bloop/releases/download/%s/install.py")
+(defconst scala-bootstrap-bloop-releases-api
+  "https://api.github.com/repos/scalacenter/bloop/releases/latest")
+
+(defun scala-bootstrap-enable-bin-directory ()
+  (let ((dir scala-bootstrap:bin-directory))
+    (make-directory dir t)
+    (add-to-list 'exec-path dir)
+    (unless (member dir (split-string (getenv "PATH") ":" t))
+      (setenv "PATH" (concat dir ":" (getenv "PATH"))))))
+
+(defun scala-bootstrap-process-buffer-name (process-name)
+  (format "*scala-bootstrap:%s*" process-name))
+
+(defun scala-bootstrap-async-command (name command callback)
+  (let* ((buf-name (scala-bootstrap-process-buffer-name name))
+         (buf (get-buffer-create buf-name))
+         (win (selected-window)))
+    (make-process
+     :name name
+     :buffer buf-name
+     :command command
+     :stderr buf
+     :sentinel `(lambda (proc event)
+                  (when (and (eq (process-status proc) 'exit)
+                             (= 0 (process-exit-status proc)))
+                    (funcall ',callback proc))))))
+
+;; metals
+
+;;;###autoload
+(defun scala-bootstrap:maybe-async-install-coursier (callback)
+  (if (executable-find "coursier")
+      (funcall callback)
+    (let ((output (expand-file-name "coursier" scala-bootstrap:bin-directory)))
+      (scala-bootstrap-async-command
+       "install-coursier"
+       (list "curl" "-vL" "-o"
+             (shell-quote-argument output)
+             "https://git.io/coursier")
+       `(lambda (&rest args)
+          (call-process-shell-command
+           (format "chmod a+x %s" (shell-quote-argument ,output)))
+          (funcall ',callback))))))
+
+(defun scala-bootstrap-parse-metals-version-from-json (json)
+  (let* ((tag (gethash "tag_name" json)))
+    (string-remove-prefix "v" tag)))
+
+(defun scala-bootstrap-async-metals-latest-version (callback)
+  (scala-bootstrap-async-command
+   "install-metals"
+   (list "curl" "-sL" scala-bootstrap-metals-releases-api)
+   `(lambda (proc)
+      (let* ((buf (process-buffer proc))
+             (version
+              (with-current-buffer (prog1 buf
+                                     (unless (buffer-live-p buf)
+                                       (error "No response buffer")))
+                (save-excursion
+                  (goto-char (point-min))
+                  (let* ((json-object-type 'hash-table)
+                         (json-array-type 'list)
+                         (json-key-type 'string)
+                         (json (ignore-errors (json-read))))
+                    (scala-bootstrap-parse-metals-version-from-json json))))))
+        (message "The latest version of Metals: %s" version)
+        (funcall ',callback
+                 (format scala-bootstrap-metals-artifact-template
+                         scala-bootstrap:metals-scala-version
+                         version))))))
+
+(defun scala-bootstrap-metals-binary ()
+  (expand-file-name "metals-emacs" scala-bootstrap:bin-directory))
+
+;;;###autoload
+(defun scala-bootstrap:maybe-async-install-metals (callback)
+  (if (executable-find "metals-emacs")
+      (funcall callback)
+    (let ((install-fun
+           `(lambda (artifact)
+              (let ((callback ',callback)
+                    (output (scala-bootstrap-metals-binary)))
+                (message "Install %s to %s" artifact output)
+                (scala-bootstrap-async-command
+                 "install-metals"
+                 (list "coursier" "bootstrap"
+                       "--java-opt" "-Xss4m"
+                       "--java-opt" "-Xms100m"
+                       "--java-opt" "-Dmetals.client=emacs"
+                       artifact
+                       "-r" "bintray:scalacenter/releases"
+                       "-r" "sonatype:snapshots"
+                       "-o" output "-f")
+                 `(lambda (&rest args) (funcall ',callback)))))))
+      (if scala-bootstrap:metals-version
+          (funcall install-fun scala-bootstrap:metals-version)
+        (scala-bootstrap-async-metals-latest-version install-fun)))))
+
+;;;###autoload
+(defun scala-bootstrap:reinstall-metals ()
+  "Reinstall Metals."
+  (interactive)
+  (call-process-shell-command
+   (format "rm -f %s" (shell-quote-argument (scala-bootstrap-metals-binary))))
+  (scala-bootstrap:maybe-async-install-metals 'ignore))
+
+;; bloop
+
+(defun scala-bootstrap-parse-bloop-installer-from-json (json)
+  (let* ((assets (gethash "assets" json))
+         (download-urls (mapcar #'(lambda (asset)
+                                    (gethash "browser_download_url" asset))
+                                assets)))
+    (car-safe
+     (seq-filter (lambda (s)
+                   (string-suffix-p "/install.py" s))
+                 download-urls))))
+
+(defun scala-bootstrap-async-bloop-latest-version (callback)
+  (scala-bootstrap-async-command
+   "install-bloop"
+   (list "curl" "-sL" scala-bootstrap-bloop-releases-api)
+   `(lambda (proc)
+      (let* ((buf (process-buffer proc))
+             (installer
+              (with-current-buffer (prog1 buf
+                                     (unless (buffer-live-p buf)
+                                       (error "No response buffer")))
+                (save-excursion
+                  (goto-char (point-min))
+                  (let* ((json-object-type 'hash-table)
+                         (json-array-type 'list)
+                         (json-key-type 'string)
+                         (json (ignore-errors (json-read))))
+                    (scala-bootstrap-parse-bloop-installer-from-json json))))))
+        (message "The latest installer of bloop: %s" installer)
+        (funcall ',callback installer)))))
+
+(defun scala-bootstrap-bloop-binary ()
+  (expand-file-name "bloop" scala-bootstrap:bin-directory))
+
+;;;###autoload
+(defun scala-bootstrap:maybe-async-install-bloop (callback)
+  (if (executable-find "bloop")
+      (funcall callback)
+    (let ((install-fun
+           `(lambda (installer-url)
+              (let* ((callback ',callback)
+                     (output (scala-bootstrap-bloop-binary))
+                     (dest scala-bootstrap:bin-directory)
+                     (cmds (list
+                            (format "curl -sL %s"
+                                    (shell-quote-argument installer-url))
+                            (format "python - --dest %s"
+                                    (shell-quote-argument dest))))
+                     (shell-cmd (mapconcat #'identity cmds " | ")))
+                (message "Install bloop to %s" output)
+                (scala-bootstrap-async-command
+                 "install-bloop"
+                 (list shell-file-name shell-command-switch shell-cmd)
+                 `(lambda (&rest args) (funcall ',callback)))))))
+      (if scala-bootstrap:bloop-version
+          (let ((version
+                 (if (string-prefix-p "v" scala-bootstrap:bloop-version)
+                     scala-bootstrap:bloop-version
+                   (concat "v" scala-bootstrap:bloop-version))))
+            (funcall install-fun
+                     (format scala-bootstrap-bloop-installer-url-template
+                             version)))
+        (scala-bootstrap-async-bloop-latest-version install-fun)))))
+
+;;;###autoload
+(defun scala-bootstrap:reinstall-bloop ()
+  "Reinstall Bloop."
+  (interactive)
+  (call-process-shell-command
+   (format "rm -f %s" (shell-quote-argument (scala-bootstrap-bloop-binary))))
+  (scala-bootstrap:maybe-async-install-bloop 'ignore))
+
+;;;###autoload
+(defun scala-bootstrap:start-bloop-server ()
+  (message "Start bloop server")
+  (start-process
+   (scala-bootstrap-process-buffer-name "bloop-server")
+   "*bloop-server*"
+   "bloop" "server"))
+
+;; syntax
+
+;;;###autoload
+(defmacro scala-bootstrap:with-metals-installed (&rest body)
+  "Ensure that Metals is installed.  It is installed to a
+directory specified by `scala-bootstrap:bin-directory', which
+defaults to '~/.emacs.d/bin'.
+
+If `scala-bootstrap:metals-version' is non-nill, that version of
+Metals will be installed.  Otherwise, the latest version will be
+installed.
+
+It also installs Coursier binary in background, which is needed
+to install Metals."
+  `(let ((buf (current-buffer)) (body ',body))
+     (scala-bootstrap-enable-bin-directory)
+     (scala-bootstrap:maybe-async-install-coursier
+      `(lambda ()
+         (let ((buf ,buf) (body ',body))
+         (scala-bootstrap:maybe-async-install-metals
+          `(lambda ()
+             (with-current-buffer ,buf
+               ,@body))))))))
+
+;;;###autoload
+(defmacro scala-bootstrap:with-bloop-installed (&rest body)
+  "Ensure that Bloop is installed.  It is installed to a
+directory specified by `scala-bootstrap:bin-directory', which
+defaults to '~/.emacs.d/bin'.
+
+If `scala-bootstrap:bloop-version' is non-nill, that version of
+Bloop will be installed.  Otherwise, the latest version will be
+installed."
+  `(let ((buf (current-buffer)) (body ',body))
+     (scala-bootstrap-enable-bin-directory)
+     (scala-bootstrap:maybe-async-install-bloop
+      `(lambda ()
+         (with-current-buffer ,buf
+           ,@body)))))
+
+;;;###autoload
+(defmacro scala-bootstrap:with-bloop-server-started (&rest body)
+  "Ensure that Bloop server is started.  If there is one
+somewhere on your machine, it does nothing.  Otherwise, a server
+will be started as a process which appears in `list-processes'
+and whose logs appear in '*bloop-server*' buffer.
+
+This macro also ensures that Bloop is installed before starting a
+server.
+"
+  `(scala-bootstrap:with-bloop-installed
+    (unless (= 0 (call-process-shell-command "bloop about"))
+      (scala-bootstrap:start-bloop-server))
+    ,@body))
+
+(provide 'scala-bootstrap)
+;;; scala-bootstrap.el ends here
